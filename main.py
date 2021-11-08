52217655e62ddfba1d33de3d1fdd7bc3a6bcb45d
johngi-thb <john.gilbert.br@gmail.com>
2021-11-08 15:04:31 +0100

    added merge of vegetation difference and function to check if there is a new image

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ import json
 import numpy as np
 from PIL import Image
 import urllib
-# import FireHR for high resolution images; install of package does not work yet
+# import FireHR for high resolution images; install of package only works on linux
+
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
 
 # ee.Authenticate()
 ee.Initialize()
@@ -87,6 +90,33 @@ def save_as_jpg(url, filename):
     img_array = np.load(array_file[0])
     img_jpg = Image.fromarray(img_array.astype(np.uint8))
     img_jpg.save(f'{ filename }.jpg')
+    img_jpg.show()
+
+def change_image_color_and_merge(images, colors):
+    img1 = Image.open(images[0])
+    img2 = Image.open(images[1])
+    img1 = img1.convert("RGB")
+    img2 = img2.convert("RGB")
+
+    data1 = img1.getdata()
+    data2 = img2.getdata()
+    new_image_data = []
+    for item1, item2 in zip(data1, data2):
+        # change all white (also shades of whites) pixels to yellow
+        if 190 < item1[0] < 256:
+            new_image_data.append(colors[0])
+        elif 190 < item2[0] < 256:
+            new_image_data.append(colors[1])
+        else:
+            new_image_data.append(item1)
+    # update image data
+    img1.putdata(new_image_data)
+
+    # show image in preview
+    # img1.show()
+
+    img1.save('growth_decline.jpg')
+
 
 # download image collection for the whole range of dates
 collection = (ee.ImageCollection('COPERNICUS/S2')
@@ -94,7 +124,23 @@ collection = (ee.ImageCollection('COPERNICUS/S2')
               .filterBounds(geometry)
               .map(lambda image: image.clip(geometry))
               .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 1)))
+latest_image = ee.Image(collection.toList(collection.size()).get(collection.size().subtract(1)))
+latest_image_date = latest_image.date().format("YYYY-MM-dd").getInfo()
 
+
+# compare date of latest image with last recorded image
+# not complete yet
+# with open('data.json', 'r+', encoding='utf-8') as f:
+#     d = json.load(f)
+#     if 'latest_image' in d:
+#         previous_image_date = datetime.datetime.strptime(d['latest_image'], '%Y-%m-%d')
+#     else:
+#         print('No new data available.')
+#     if datetime.datetime.strptime(latest_image_date, '%Y-%m-%d') > previous_image_date:
+#         print('New data available.')
+#         data = {'latest_image': latest_image_date}
+#         json.dump(data, f, ensure_ascii=False, indent=4)
+#     f.close()
 # select images from collection
 ndvi_collection = collection.map(add_NDVI)
 ndvi_img_start = ee.Image(ndvi_collection.toList(ndvi_collection.size()).get(0))
@@ -110,132 +156,16 @@ growth_img = ndvi_img_end.select('thres').subtract(ndvi_img_start.select('thres'
 growth_url = growth_img.getDownloadURL({'format': 'NPY'})
 decline_url = decline_img.getDownloadURL({'format': 'NPY'})
 
+
 # save JPG's
 save_as_jpg(decline_url, 'decline')
+save_as_jpg(growth_url, 'growth')
+# change color of jpg's
+change_image_color_and_merge(['growth.jpg', 'decline.jpg'], [(0, 255, 0), (255, 0, 0)])
+
 
 
 # TODO: if new data then do analysis condition
 # TODO: run analayis over the 4 data sets with the dynamic dates
 # TODO: save output maps and stats to disk but discard raw data
 # TODO: chart changes changes over time
-
-
-### SCRAP!!!!####
-# def format_function(table, row_id, col_id):
-#     rows = table.distinct(row_id)
-#     joined = ee.Join.saveAll('matches').apply(
-#         primary=rows,
-#         secondary=table,
-#         condition=ee.Filter.equals(
-#             leftField=row_id,
-#             rightField=row_id
-#         )
-#     )
-#     return joined.map(lambda row: ((value := ee.List(row.get('matches'))
-#                                     .map(lambda feature: (feature := ee.Feature(feature),
-#                                                           [feature.get(col_id), feature.get('ndvi')])[-1])))[-1])
-# def merge_function(table, row_id):
-#     return table.map(lambda feature: (id := feature.get(row_id),
-#                                       all_keys := feature.toDictionary().keys().remove(row_id),
-#                                       substr_keys := ee.List(all_keys.map(lambda val: ee.String(val).slice(0, 8))),
-#                                       unique_keys := substr_keys.distinct(),
-#                                       pairs := unique_keys.map(lambda key: (matches := feature.toDictionary().select(all_keys)))))
-
-# ------> ee.Reducer  not recognized by by Python <-----
-# triplets = ndvi_collection.map(lambda image: image.select('ndvi').reduceRegions(
-#     collection=geometry,
-#     reducer=ee.Reducer.first().setOutputs(['ndvi']),
-#     scale=10
-# )).map(lambda feature: (ndvi := ee.List([feature.get('ndvi'), -9999])).reduce(ee.Reducer.firstNonNull(),
-#                                                                               feature.set(
-#                                                                                   {'ndvi': ndvi,
-#                                                                                    'imageID': image.id()}
-#                                                                               )))[-1].flatten()
-
-# format = format_function
-# sentinelResults = format(triplets, 'id', 'imageID')
-
-
-# Map data
-# geometry = data['features'][0]['geometry']['coordinates']
-
-# get timeframe specified by user as variable
-# possible options:
-## july 2016, november 2016, last year, two weeks
-
-# function get newest date from month
-# parameter: month, region
-## return latest image date
-
-# function new image data available
-# params (timeframe identifier if july 2016, november 2016, last year, two weeks); data file; region
-# TODO
-## if identifier july2016
-#### compare newest july data to last july data from file; return true/false
-## if identifier november2016
-#### compare newest november data to last july data from file; return true/false
-## if identifier last year
-#### compare last year to most recent date in file return true/false
-
-## if identifier two weeks
-#### compare newest date to most recent date in file return true/false
-
-# function get satellite pictures to compare
-# parameters: (identifier if july 2016, november 2016, last year, two weeks); data file; region
-## if new image data available
-#### if identifier july2016
-###### update datafile
-###### return july 2016 and current july
-#### if identifier november2016
-###### update datafile
-###### return november2016 and current november
-#### if identifier last year
-###### update datafile
-###### return last year image and newest image
-#### if identifier two weeks
-###### update datafile
-###### return image two weeks ago and newest image
-
-
-# function to mask cloud and shadows
-# param: image
-# return: updated image without cloud and shadows
-
-# function to add NDVI and calculate area
-# param: image
-# return: image with ndvi
-
-# TODO
-# function get cloud cover
-# parameter image
-## return cloud cover percentage
-
-# check if new image data available with the according function
-# if no new satellite data:
-## do nothing
-# if new satellite data:
-## get satellite pictures to compare and save them in variables
-
-# TODO
-# call cloud cover function
-# if cloud cover  over 30%:
-## no report
-# else  comment cloud cover percentage in report
-
-# get satellite pictures to compare
-# apply ndvi function to start date and end date image
-# apply cloud mask to start date and end date image
-# create triplets
-# format triplets
-# merge images
-
-# calculate cover in %
-# calculate change
-# data tables of the raw numbers in the report probably with pandas
-
-# create image from difference between start and end image possibly with the Python Image Library
-# or with the thumbnail function in gee
-
-# save image as png save the filename to the data file so we can later generate a pdf from the generated images
-
-# send email with pdf via smtpd library
