ddc3f69a4f60e423230093fe2a17a830d7536275
johngi-thb <john.gilbert.br@gmail.com>
2021-12-27 11:27:56 +0100

    changed parameter to fix another formatting issue

--- main.py
+++ main.py
@@ -254,24 +254,24 @@ def generate_pdf(data, pdf_name):
         pdf.set_font('Arial', 'B',  size=26)
         pdf.cell(
             txt=f'{data[timeframe]["project_name"]}: {head_text[timeframe]} vegetation evaluation ({data[timeframe]["start_date_satellite"]} to {data[timeframe]["end_date_satellite"]})',
-            ln=1, w=0)
+            ln=2, w=0)
         pdf.set_font('Arial', size=24)
-        pdf.cell(txt=f'Project area: {data[timeframe]["project_area"]:.2f} km²', ln=1, w=0)
+        pdf.cell(txt=f'Project area: {data[timeframe]["project_area"]:.2f} km²', ln=2, w=0)
         pdf.cell(
             txt=f'Vegetation cover ({data[timeframe]["start_date"]}): {data[timeframe]["vegetation_start"]:,} m² ({data[timeframe]["vegetation_share_start"]:.2f}%)',
-            ln=1, w=0)
+            ln=2, w=0)
         pdf.cell(
             txt=f'Vegetation cover ({data[timeframe]["end_date"]}): {data[timeframe]["vegetation_end"]:,} m² ({data[timeframe]["vegetation_share_end"]:.2f}%)',
-            ln=1, w=0)
+            ln=2, w=0)
         pdf.cell(
             txt=f'Net vegetation change: {data[timeframe]["area_change"]:,} m² ({data[timeframe]["vegetation_share_change"]:.2f}%)',
-            ln=1, w=0)
+            ln=2, w=0)
         pdf.cell(
             txt=f'Vegetation gain: {data[timeframe]["vegetation_gain"]:,} m² ({data[timeframe]["vegetation_gain_relative"]:.2f}%)',
-            ln=1, w=0)
+            ln=2, w=0)
         pdf.cell(
             txt=f'Vegetation loss: {data[timeframe]["vegetation_loss"]:,} m² ({data[timeframe]["vegetation_loss_relative"]:.2f}%)',
-            ln=1, w=0)
+            ln=2, w=0)
         pdf.image(data[timeframe]["path"], x=0, y=200, w=1200, h=1200)
     pdf.output(pdf_name)
 
